3485281714c3ed2dec4c35caabb0cc88a4967ce6
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-07-31 12:38:44 +0530

    Add files via upload

---
+++ spring-boot-security-form-auth-remember-persistence-token/src/main/java/com/jeejava/spring/boot/security/form/auth/remember/persistence/token/config/DatabaseConfig.java
@@ -0,0 +1,31 @@
+package com.jeejava.spring.boot.security.form.auth.remember.persistence.token.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+@Configuration
+public class DatabaseConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.H2) // .H2 or .DERBY, etc.
+				.addScript("user.sql").addScript("user-role.sql").addScript("persistent_logins.sql").build();
+		return db;
+	}
+
+	@Bean
+	public PersistentTokenRepository persistentTokenRepository() {
+		JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+		tokenRepository.setDataSource(dataSource());
+		return tokenRepository;
+	}
+
+}
