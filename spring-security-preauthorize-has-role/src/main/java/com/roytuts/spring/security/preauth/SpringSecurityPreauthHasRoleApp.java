c510f6b5a3e577e7a79a3c3fc183d06261aff183
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2020-02-07 13:13:54 +0530

    Create SpringSecurityPreauthHasRoleApp.java

---
+++ spring-security-preauthorize-has-role/src/main/java/com/roytuts/spring/security/preauth/SpringSecurityPreauthHasRoleApp.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.security.preauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPreauthHasRoleApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPreauthHasRoleApp.class, args);
+	}
+
+}
