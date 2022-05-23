ecff182cb93f6db3a440ab63fa22dbc3af0c3e59
Soumitra <projwebinfo@gmail.com>
2022-05-23 11:43:43 +0530

    Create UserRole.java

---
+++ spring-preauthorize-hasrole-data-jpa/src/main/java/com/roytuts/spring/preauthorize/hasrole/data/jpa/entity/UserRole.java
@@ -0,0 +1,52 @@
+package com.roytuts.spring.preauthorize.hasrole.data.jpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_role")
+public class UserRole {
+
+	@Id
+	@Column(name = "role_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer roleId;
+
+	@Column(name = "user_role")
+	private String userRole;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Integer getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(Integer roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
