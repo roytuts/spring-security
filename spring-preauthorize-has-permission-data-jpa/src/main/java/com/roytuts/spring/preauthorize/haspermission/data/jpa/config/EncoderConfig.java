6aba3d5638d204600bfe51548d2793b940b8f581
Soumitra <projwebinfo@gmail.com>
2022-06-13 16:42:45 +0530

    Create EncoderConfig.java

---
+++ spring-preauthorize-has-permission-data-jpa/src/main/java/com/roytuts/spring/preauthorize/haspermission/data/jpa/config/EncoderConfig.java
@@ -0,0 +1,16 @@
+package com.roytuts.spring.preauthorize.haspermission.data.jpa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class EncoderConfig {
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
