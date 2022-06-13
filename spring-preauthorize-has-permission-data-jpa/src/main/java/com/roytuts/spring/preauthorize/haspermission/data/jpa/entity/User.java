6fe1914c60069d36ffac74068af8c8a2012200f4
Soumitra <projwebinfo@gmail.com>
2022-06-13 16:44:22 +0530

    Create User.java

---
+++ spring-preauthorize-has-permission-data-jpa/src/main/java/com/roytuts/spring/preauthorize/haspermission/data/jpa/entity/User.java
@@ -0,0 +1,73 @@
+package com.roytuts.spring.preauthorize.haspermission.data.jpa.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	@Column(name = "user_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer userId;
+
+	@Column(name = "user_name")
+	private String userName;
+
+	@Column(name = "user_pass")
+	private String userPass;
+
+	private boolean enable;
+
+	@OneToMany(mappedBy = "user")
+	private List<UserRole> userRoles;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPass() {
+		return userPass;
+	}
+
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+
+	public boolean isEnable() {
+		return enable;
+	}
+
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+
+	public List<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(List<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+
+}
