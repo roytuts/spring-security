dc5fd6959733fbf77934ef82c22b03464f46aa29
Soumitra <projwebinfo@gmail.com>
2022-10-12 22:10:19 +0530

    Create App.java

---
+++ spring-boot-security-form-based-jdbc-userdetailsservice-auth/src/main/java/com/roytuts/spring/boot/security/form/based/jdbc/userdetailsservice/auth/App.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.boot.security.form.based.jdbc.userdetailsservice.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
