935208fce272b19e3d0422b60161b367267fb53a
Soumitra <projwebinfo@gmail.com>
2022-10-27 12:42:05 +0530

    Create SecurityWebApplicationInitializer.java

---
+++ spring-security-annotations/src/main/java/com/roytuts/spring/security/annotations/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.roytuts.spring.security.annotations.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
