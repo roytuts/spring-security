77574a1e50b381f375c6affe43ad37aaf4e184c2
Soumitra <projwebinfo@gmail.com>
2022-10-31 16:09:53 +0530

    Create MvcWebApplicationInitializer.java

---
+++ spring-security-jdbc-authentication-userdetailsservice/src/main/java/com/roytuts/spring/security/jdbc/authentication/config/MvcWebApplicationInitializer.java
@@ -0,0 +1,22 @@
+package com.roytuts.spring.security.jdbc.authentication.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { WebSecurityConfig.class, WebMvcConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
