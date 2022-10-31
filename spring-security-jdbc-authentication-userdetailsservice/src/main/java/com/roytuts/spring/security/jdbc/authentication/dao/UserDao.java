23fcc4acbfae6f9f8cc1d7f154cae8bcabcdc85b
Soumitra <projwebinfo@gmail.com>
2022-10-31 16:07:02 +0530

    Create UserDao.java

---
+++ spring-security-jdbc-authentication-userdetailsservice/src/main/java/com/roytuts/spring/security/jdbc/authentication/dao/UserDao.java
@@ -0,0 +1,30 @@
+package com.roytuts.spring.security.jdbc.authentication.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.roytuts.spring.security.jdbc.authentication.model.UserDetails;
+import com.roytuts.spring.security.jdbc.authentication.rowmapper.UserRowMapper;
+
+@Repository
+@Transactional
+public class UserDao {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public UserDetails getUser(String username) {
+		try {
+			final String sql = "select u.user_name user_name, u.user_pass user_pass, ur.user_role user_role from user u, user_role ur where u.user_name = ? and u.user_name = ur.user_name";
+
+			UserDetails userDetails = jdbcTemplate.queryForObject(sql, new UserRowMapper(), username);
+
+			return userDetails;
+		} catch (EmptyResultDataAccessException ex) {
+			return null;// should have proper handling of Exception
+		}
+	}
+}
