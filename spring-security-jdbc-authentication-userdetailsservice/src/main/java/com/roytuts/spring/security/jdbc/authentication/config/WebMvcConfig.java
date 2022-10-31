af60305e20360c5c566d83e3af862fdf5160a20b
Soumitra <projwebinfo@gmail.com>
2022-10-31 16:10:57 +0530

    Create WebMvcConfig.java

---
+++ spring-security-jdbc-authentication-userdetailsservice/src/main/java/com/roytuts/spring/security/jdbc/authentication/config/WebMvcConfig.java
@@ -0,0 +1,56 @@
+package com.roytuts.spring.security.jdbc.authentication.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@PropertySource(value = { "classpath:messages.properties", "classpath:jdbc.properties" })
+@ComponentScan(basePackages = "com.roytuts.spring.security.jdbc.authentication.controllers")
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private Environment environment;
+
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver("/views/", ".jsp");
+		return viewResolver;
+	}
+
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+
+	@Bean("messageSource")
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	@Bean(name = "dataSource")
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+		dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+		dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+		return dataSource;
+	}
+
+}
