b8ff4d557d7c3d858b2e22c2246c013b785d3d62
Soumitra <projwebinfo@gmail.com>
2022-11-02 16:36:49 +0530

    Create SecurityWebApplicationInitializer.java

---
+++ spring-security-jdbc-authentication-persistent-token-remember-me/src/main/com/roytuts/spring/security/jdbc/authentication/persistent/rememberme/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.roytuts.spring.security.jdbc.authentication.persistent.rememberme.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
