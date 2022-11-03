5cf2c1fca3f115c40a18eb1612fa381a94c6c4cf
Soumitra <projwebinfo@gmail.com>
2022-11-03 18:02:13 +0530

    Update SpringPreAuthenticationConfig.java

--- spring-security-pre-authentication/src/main/java/com/roytuts/spring/security/preauth/config/SpringPreAuthenticationConfig.java
+++ spring-security-pre-authentication/src/main/java/com/roytuts/spring/security/preauth/config/SpringPreAuthenticationConfig.java
@@ -2,20 +2,34 @@ package com.roytuts.spring.security.preauth.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class SpringPreAuthenticationConfig {
+	
+	@Autowired
+	private Environment environment;
 
-	@Bean
+	/*@Bean
 	public DataSource dataSource() {
 		EmbeddedDatabaseBuilder embeddedDatabaseBuilder = new EmbeddedDatabaseBuilder();
 		return embeddedDatabaseBuilder.addScript("classpath:create-table.sql").addScript("insert-data.sql")
 				.setType(EmbeddedDatabaseType.H2).build();
+	}*/
+	
+	@Bean
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+		dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+		dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+		return dataSource;
 	}
 
 	@Bean
