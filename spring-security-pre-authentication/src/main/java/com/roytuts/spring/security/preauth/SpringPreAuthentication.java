6977870c5e216ff17f845aac35e3241a815135d5
Soumitra <projwebinfo@gmail.com>
2022-11-03 18:01:10 +0530

    Update SpringPreAuthentication.java

--- spring-security-pre-authentication/src/main/java/com/roytuts/spring/security/preauth/SpringPreAuthentication.java
+++ spring-security-pre-authentication/src/main/java/com/roytuts/spring/security/preauth/SpringPreAuthentication.java
@@ -3,7 +3,7 @@ package com.roytuts.spring.security.preauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.roytuts.spring.security.preauth")
+@SpringBootApplication
 public class SpringPreAuthentication {
 
 	public static void main(String[] args) {
