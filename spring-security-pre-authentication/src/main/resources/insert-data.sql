1f3077160bd8553678751f0ab2464d41fe0aeadc
Soumitra <projwebinfo@gmail.com>
2022-11-03 17:59:48 +0530

    Update insert-data.sql

--- spring-security-pre-authentication/src/main/resources/insert-data.sql
+++ spring-security-pre-authentication/src/main/resources/insert-data.sql
@@ -1,2 +1,2 @@
-insert  into `user`(`user_name`,`user_pass`,`enable`) values ('roy','ae685575101ee7165c90a8f2c30c6e60cdd9e482',1);
+insert  into `user`(`user_name`,`user_pass`,`enable`) values ('roy','$2a$12$PN0MjtyNGWW.AjWdIuYKxe4.4Grjs4K7oanuAnt/WSDSvFlUc3eQi',1);
 insert  into `user_role`(`user_name`,`user_role`) values ('roy','ROLE_ADMIN');
