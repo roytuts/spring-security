c6b4e6ac96a923be33ea3c79d44b834250e822a0
Soumitra <projwebinfo@gmail.com>
2022-11-03 17:58:14 +0530

    Create PreAuthenticationSecurityConfig.java

---
+++ spring-security-pre-authentication/src/main/java/com/roytuts/spring/security/preauth/config/PreAuthenticationSecurityConfig.java
@@ -0,0 +1,67 @@
+package com.roytuts.spring.security.preauth.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
+
+import com.roytuts.spring.security.preauth.service.CustomUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class PreAuthenticationSecurityConfig {
+
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.addFilterAfter(siteminderFilter(), RequestHeaderAuthenticationFilter.class).authorizeRequests()
+				.antMatchers("/static/**").permitAll().antMatchers("/").permitAll().antMatchers("/blogs")
+				.access("hasRole('ADMIN')").anyRequest().authenticated();
+
+		return http.build();
+	}
+
+	@Bean(name = "siteminderFilter")
+	public RequestHeaderAuthenticationFilter siteminderFilter() throws Exception {
+		RequestHeaderAuthenticationFilter requestHeaderAuthenticationFilter = new RequestHeaderAuthenticationFilter();
+		requestHeaderAuthenticationFilter.setPrincipalRequestHeader("SM_USER");
+		requestHeaderAuthenticationFilter.setAuthenticationManager(authenticationManager());
+		return requestHeaderAuthenticationFilter;
+	}
+
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		final List<AuthenticationProvider> providers = new ArrayList<>(1);
+		providers.add(preauthAuthProvider());
+		return new ProviderManager(providers);
+	}
+
+	@Bean(name = "preAuthProvider")
+	PreAuthenticatedAuthenticationProvider preauthAuthProvider() throws Exception {
+		PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
+		provider.setPreAuthenticatedUserDetailsService(userDetailsServiceWrapper());
+		return provider;
+	}
+
+	@Bean
+	UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> userDetailsServiceWrapper() throws Exception {
+		UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> wrapper = new UserDetailsByNameServiceWrapper<>();
+		wrapper.setUserDetailsService(customUserDetailsService);
+		return wrapper;
+	}
+
+}
