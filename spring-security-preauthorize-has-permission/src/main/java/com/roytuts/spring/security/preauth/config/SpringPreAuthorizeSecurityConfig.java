969f11ec9a43c5e31691e1bf41337504c7e0be26
Soumitra <projwebinfo@gmail.com>
2023-10-06 18:35:11 +0530

    Update SpringPreAuthorizeSecurityConfig.java

--- spring-security-preauthorize-has-permission/src/main/java/com/roytuts/spring/security/preauth/config/SpringPreAuthorizeSecurityConfig.java
+++ spring-security-preauthorize-has-permission/src/main/java/com/roytuts/spring/security/preauth/config/SpringPreAuthorizeSecurityConfig.java
@@ -6,38 +6,35 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.roytuts.spring.security.preauth.permission.CustomPermissionEvaluator;
 import com.roytuts.spring.security.preauth.service.UserAuthService;
 
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SpringPreAuthorizeSecurityConfig extends WebSecurityConfigurerAdapter {
+\@Configuration
+public class SpringPreAuthorizeSecurityConfig {
 
 	@Autowired
 	private CustomPermissionEvaluator permissionEvaluator;
 	
 	@Autowired
 	private UserAuthService userAuthService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	/*@Autowired
 	public void registerGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		// Ideally database authentication is required
-		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("user")
+		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder).withUser("user")
 				.password("$2a$10$9Xn39aPf4LhDpRGNWvDFqu.T5ZPHbyh8iNQDSb4aNSnLqE2u2efIu").roles("USER").and()
-				.passwordEncoder(passwordEncoder()).withUser("admin")
+				.passwordEncoder(passwordEncoder).withUser("admin")
 				.password("$2a$10$dl8TemMlPH7Z/mpBurCX8O4lu0FoWbXnhsHTYXVsmgXyzagn..8rK").roles("USER", "ADMIN");
 	}*/
 	
 	@Autowired
 	public void registerGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userAuthService).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(userAuthService).passwordEncoder(passwordEncoder);
 	}
 
 	@Bean
@@ -45,11 +42,6 @@ public class SpringPreAuthorizeSecurityConfig extends WebSecurityConfigurerAdapt
 		DefaultMethodSecurityExpressionHandler handler = new DefaultMethodSecurityExpressionHandler();
 		handler.setPermissionEvaluator(permissionEvaluator);
 		return handler;
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 
 }
