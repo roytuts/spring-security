9aa6429bed08d565f17f9e59a78ab2bba49ae6d3
Soumitra <projwebinfo@gmail.com>
2023-10-06 18:34:34 +0530

    Update SpringSecurityPreauthHasPermissionApp.java

--- spring-security-preauthorize-has-permission/src/main/java/com/roytuts/spring/security/preauth/SpringSecurityPreauthHasPermissionApp.java
+++ spring-security-preauthorize-has-permission/src/main/java/com/roytuts/spring/security/preauth/SpringSecurityPreauthHasPermissionApp.java
@@ -2,9 +2,8 @@ package com.roytuts.spring.security.preauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = WebMvcAutoConfiguration.class)
+@SpringBootApplication
 public class SpringSecurityPreauthHasPermissionApp {
 
 	public static void main(String[] args) {
