18a64ec68c358f00b9fa998f4eb63a15da60a415
Soumitra <projwebinfo@gmail.com>
2023-10-16 19:03:17 +0530

    Create SpringBootAngularSecurityApp.java

---
+++ springboot-angular-security/src/main/java/com/roytuts/springboot/angularsecurity/SpringBootAngularSecurityApp.java
@@ -0,0 +1,13 @@
+package com.roytuts.springboot.angularsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularSecurityApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularSecurityApp.class, args);
+	}
+
+}
