20290e331b6ee68f2c6e4057115a64122d9efa95
Soumitra <projwebinfo@gmail.com>
2023-12-04 19:35:28 +0530

    Create SpringBootReactSecurityApp.java

---
+++ springboot-react-security/src/main/java/com/roytuts/springboot/reactsecurity/SpringBootReactSecurityApp.java
@@ -0,0 +1,13 @@
+package com.roytuts.springboot.reactsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactSecurityApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactSecurityApp.class, args);
+	}
+
+}
